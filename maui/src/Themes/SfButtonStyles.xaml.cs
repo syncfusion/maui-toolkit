66deeb461a7db37a8e6dcaf30cb9565226e86efc
Choza-rajan <chozarajan.pandiyarajan@syncfusion.com>
2024-12-04 15:03:22 +0530

    The new SfButton control is committed

---
+++ maui/src/Themes/SfButtonStyles.xaml.cs
@@ -0,0 +1,15 @@
+namespace Syncfusion.Maui.Toolkit.Buttons;
+
+/// <summary>
+/// SfButtonStyles class provides a set of predefined styles for SfButton control.
+/// </summary>
+public partial class SfButtonStyles : ResourceDictionary
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="SfButtonStyles"/> class.
+	/// </summary>
+	public SfButtonStyles()
+	{
+		InitializeComponent();
+	}
+}
