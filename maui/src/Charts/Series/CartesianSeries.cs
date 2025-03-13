29e487e285d24bd2f2e42de1d1bb85bf6483bd99
subash_s <subash.s@syncfusion.com>
2025-03-13 13:48:41 +0530

    changes made

--- maui/src/Charts/Series/CartesianSeries.cs
+++ maui/src/Charts/Series/CartesianSeries.cs
@@ -601,6 +601,8 @@ namespace Syncfusion.Maui.Toolkit.Charts
             }
         }
 
+        internal virtual bool IsFillEmptyPoint { get { return true; } }
+
         #endregion
 
         #region Constructor
